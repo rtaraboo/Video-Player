d3933a6fb0a3f5a2544f638066cfe9ca1838db9d
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-10-01 16:56:36 -0400

    Initial Commit

--- Video Player/ViewController.swift
+++ Video Player/ViewController.swift
@@ -7,17 +7,39 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
+    
+    var mediaPlayer : MPMoviePlayerController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        playVideo()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func playVideo(){
+        
+        let path = NSBundle.mainBundle().pathForResource("IMG_7498", ofType: "mov")
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/u/49292460/IMG_7498.mov")
+        mediaPlayer = MPMoviePlayerController(contentURL: url)
+        
+        if let player = mediaPlayer{
+            player.view.frame = self.view.bounds
+            player.prepareToPlay()
+            player.scalingMode = .AspectFill
+            self.view.addSubview(player.view)
+            
+        }
+    
     }
 
 
